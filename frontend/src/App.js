a548e9cd51d8bae0959644d914cfae6c30f769c2
Mubbarik Ali <mubbarikali@gmail.com>
2023-10-31 14:21:00 +0500

    Added Frontend navigation and pages.

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Dashboard from './pages/Dashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <>
+  
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Dashboard />} ></Route>
+            <Route path='/register' element={<Register />} ></Route>
+            <Route path='/login' element={<Login />} ></Route>
+          </Routes>
+        </div>
+      </Router>
+    
+    </>
+  );
+}
+
+export default App;
