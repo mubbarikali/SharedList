872dda0b7bf9bc317350f165f148c672c35036ec
Mubbarik Ali <mubbarikali@gmail.com>
2023-11-10 14:57:21 +0500

    created build dist in frontend for production

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const itemsRouter = require('./routes/itemRoutes');
 const userRoutes = require('./routes/userRoutes');
@@ -16,6 +17,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/v1/items/", itemsRouter);
 app.use("/api/v1/users", userRoutes);
+
+
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 
 
 // app.use(errorHandler);
